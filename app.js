266fa341cd783904c1ee52c069efa7eb6f080043
espensyversen-groupaccounting <espen.syversen@strawberry.no>
2026-05-22 11:39:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 // Versjon – må matche APP_VERSION i service-worker.js
-const APP_VERSION = '1.0.4';
+const APP_VERSION = '1.0.5';
 
 // Service Worker oppdateringsstatus
 let swRegistration  = null;
